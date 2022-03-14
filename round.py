1167bb23962b8d1ca0a249fb4c09739fdccf53bc
wlsgml8847 <8847wlsgml@naver.com>
2022-03-14 19:21:45 +0900

    Update round.py

--- round.py
+++ round.py
@@ -5,3 +5,12 @@ if a == 0.9:
     print(True)
 else:
     print(False)
+
+# 소수점값을 비교하는 작업이 필요한 문제라면 실수 값을 비교하지 
+
+print(round(a, 4))
+
+if round(a, 4) == 0.9:
+    print(True)
+else: 
+    print(False)
