8004a1f66cff9ba9eb0769ecc5d9215d522444f0
wlsgml8847 <8847wlsgml@naver.com>
2022-03-16 21:10:44 +0900

    Enter

--- 알고리즘/그리디/큰 수의 법칙.py
+++ 알고리즘/그리디/큰 수의 법칙.py
@@ -1,3 +1,10 @@
 # 입력 조건 : 째 줄에 N(2<= N <=1000)
-print("")
+N, M, K =map(int, input().split())
+li = list(map(int, input().split()))
+import math
+print(li)
 
+li.sort()
+print(li)
+#mx = math.max(li)
+#li.remove(mx)
