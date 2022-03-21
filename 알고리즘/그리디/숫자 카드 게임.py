eacd3bd831d631f38512e509e4e6f827c9f73ef7
jinhee8847 <dlwlsgml8847@gmail.com>
2022-03-21 20:05:46 +0900

    enter

--- 알고리즘/그리디/숫자 카드 게임.py
+++ 알고리즘/그리디/숫자 카드 게임.py
@@ -1,4 +1,14 @@
 # 2019 국가 교육기관 코딩 테스트
 
-#1. 숫자가 쓰인 카드들이 N x M
-#
+import readline
+
+
+N, M = map(int, input().split())
+max = 0
+for _ in range(N):
+    card = list(map(int, input().split()))
+    card.sort()
+    if max < card[0]:
+        max = card[0]
+
+print(max)
