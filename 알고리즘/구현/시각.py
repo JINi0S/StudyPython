678422ec5109d4d2314dc0e1eb5dda4e0fbc8491
jinhee8847 <dlwlsgml8847@gmail.com>
2022-03-25 19:43:50 +0900

    enter

--- 알고리즘/구현/시각.py
+++ 알고리즘/구현/시각.py
@@ -12,3 +12,16 @@ for i in range(num+1):
                     if(m % 10==3 or 30 <= m <40): 
                         res += 1
 print(res)
+
+#answer
+"""
+h = int(input())
+
+count = 0
+for i in range(h+1):
+    for j in range(60):
+        for k in rnage(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+print(count)
+"""
