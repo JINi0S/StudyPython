f4d7d3dae699c30d48d03050206bcaa6a1986826
jinhee8847 <dlwlsgml8847@gmail.com>
2022-03-26 17:53:51 +0900

    enter

---
+++ 알고리즘/구현/게임 개발.py
@@ -0,0 +1,11 @@
+a, b = map(int, input().split())
+start_x, start_y, look = map(int, input().split())
+li = [[0]*a for _ in range(b)]
+for i in range(b):
+    li[i] = list(map(int, input().split()))
+
+look_x = [-1, 0, 1]
+look_y = [-1, 0, 1]
+
+
+print(li)
