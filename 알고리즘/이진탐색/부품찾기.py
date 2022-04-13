9752e96fbf25adca29b2021cb046b29146fea2b6
jinhee8847 <dlwlsgml8847@gmail.com>
2022-04-13 17:16:24 +0900

    enter

---
+++ 알고리즘/이진탐색/부품찾기.py
@@ -0,0 +1,28 @@
+import sys
+m= int(input())
+
+m_array = list(map(int, input().split()))
+
+n= int(input())
+n_array = list(map(int, input().split()))
+
+m_array.sort()
+
+def binary_s(array, target, start, end):
+    if start>end:
+        return None
+    mid = (start + end) //2
+    if target == array[mid]:
+        return mid
+    elif target> array[mid]:
+        return binary_s(array, target, mid+1, end)
+    else:
+        return binary_s(array, target, start, mid-1)
+
+for i in n_array:
+    res = binary_s(m_array, i, 0, m) 
+
+    if res == None:
+        print("no")
+    else:
+        print("yes")
