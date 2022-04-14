5df8c1c19b416b3377efb5a8b13db5b9d4932284
jinhee8847 <dlwlsgml8847@gmail.com>
2022-04-14 21:06:09 +0900

    enter

--- 알고리즘/이진탐색/떡볶이떡만들기.py
+++ 알고리즘/이진탐색/떡볶이떡만들기.py
@@ -1,2 +1,25 @@
 n, m = map(int,input().split())
 height = list(map(int,input().split()))
+
+height.sort()
+max = height[-1]
+
+def bina(target, start, end):
+ 
+    sum = 0
+    mid = (start + end)//2
+    for i in height:
+        k = i - mid
+        if k>=0:
+            sum += k
+    if sum == target:
+        return mid
+    elif sum < target:
+        return bina(target, start, mid - 1)
+    else:
+        if start > end:
+                return mid
+        return bina(target, mid+1, end)
+
+res = bina(m, 0, max)
+print(res)
