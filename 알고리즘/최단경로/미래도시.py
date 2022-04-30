ad1b1227f9e13650d681084e04577b184bcf26a2
jinhee8847 <dlwlsgml8847@gmail.com>
2022-04-30 19:07:33 +0900

    enter

--- 알고리즘/최단경로/미래도시.py
+++ 알고리즘/최단경로/미래도시.py
@@ -7,10 +7,23 @@ for i in range(1, n+1):
     for j in range(1, n+1):
         if i == j:
             gra[i][j]=0
+            
 
 for _ in range(m):
     a, b = map(int, input().split())
     gra[a][b] = 1
+    gra[b][a]=1
 
 x, k =map(int, input().split())
-print(gra)
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            gra[a][b] = min(gra[a][b], gra[a][k]+gra[k][b])
+
+dis = gra[1][k]+gra[k][x]
+
+if dis >= INF:
+    print("-1")
+else:
+    print(dis)
