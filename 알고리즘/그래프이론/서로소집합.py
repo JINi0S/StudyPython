4a946bf93ce8083db2aef1cc63768433c7d699ba
jinhee8847 <dlwlsgml8847@gmail.com>
2022-05-17 16:56:59 +0900

    enter

--- 알고리즘/그래프이론/서로소집합.py
+++ 알고리즘/그래프이론/서로소집합.py
@@ -33,7 +33,7 @@ for i in range(e):
 # 각 원소가 속한 집합 출력
 print('각 원소가 속한 집합: ', end = '')
 for i in range(1, v+1):
-    print(find_parent(parent, i), end='')
+    print(find_parent(parent, i), end=' ')
 
 
 print()
@@ -41,4 +41,4 @@ print()
 # 부모 테이블 내용 출력
 print('부모 테이블:', end ='')
 for i in range(1, v+1):
-    print(parent[i], end='')
+    print(parent[i], end=' ')
