39fdd3548e7696af448c72d9efd023475b02e24f
jinhee8847 <dlwlsgml8847@gmail.com>
2022-06-01 00:07:17 +0900

    enter

--- 알고리즘/그리디/무지의먹방라이브.py
+++ 알고리즘/그리디/무지의먹방라이브.py
@@ -1,4 +1,24 @@
 # p 316
+
+
 def solution(food_times, k):
-    answer = 0
+    leng = len(food_times)
+    ready = 0
+    all_eat = False
+    for i in range(k):
+        ready = i % leng
+        if food_times[ready] != 0:
+            food_times[ready] = food_times[ready]-1
+        #else:
+        #    k = k+1
+    
+    a = all(food_times,)
+    print(food_times,a)
+    if a == True:
+        return -1
+         
+    answer = ready
+    print(answer)
     return answer
+
+solution([3,1,2], 5)
