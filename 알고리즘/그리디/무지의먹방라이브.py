9b18777a5174ba6c36ea33a52394fa3901d2c705
jinhee8847 <dlwlsgml8847@gmail.com>
2022-06-01 22:07:16 +0900

    enter

--- 알고리즘/그리디/무지의먹방라이브.py
+++ 알고리즘/그리디/무지의먹방라이브.py
@@ -1,24 +1,41 @@
 # p 316
-
+import numpy as np
 
 def solution(food_times, k):
     leng = len(food_times)
     ready = 0
     all_eat = False
+    """
     for i in range(k):
         ready = i % leng
         if food_times[ready] != 0:
             food_times[ready] = food_times[ready]-1
         #else:
         #    k = k+1
-    
-    a = all(food_times,)
+    """
+    i = 0
+    while(i < k):
+        ready = i % leng
+        if food_times[ready] != 0:
+            food_times[ready] = food_times[ready]-1
+        else:
+            k = k + 1
+        print(i, ready, food_times)
+        i = i + 1
+
+    a = np.all(food_times==0)
     print(food_times,a)
     if a == True:
         return -1
-         
-    answer = ready
+
+    while True:
+        if food_times[(ready +1) % leng] == 0:
+            ready += 1
+        else:
+            break
+    answer = ((ready +1) % leng ) + 1
+    
     print(answer)
     return answer
 
-solution([3,1,2], 5)
+solution([3,1,2], 4)
