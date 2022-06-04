3f496863e8c96f9a28c36437a607a8ffcdc3b6f8
jinhee8847 <dlwlsgml8847@gmail.com>
2022-06-04 23:31:10 +0900

    enter

---
+++ 알고리즘/DFS&BFS/특정거리의도시찾기.py
@@ -0,0 +1,9 @@
+# p339 / 18352
+n, m, k, x = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+print(graph)
